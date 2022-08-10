d921f38d70db0a20b462984131034302736996fb
iridium <2457065285@qq.com>
2022-08-10 22:14:17 +0800

    设置了导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,47 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+
 module.exports = {
   lang: 'zh-CN',
   title: 'SLICE',
   description: 'Slice 文档',
+  head: [
+    ['link', { rel: 'icon', href: '/images/icon.png' }]
+  ],
+  theme: defaultTheme({
+    navbar: [
+      // NavbarItem
+      {
+        text: '教程',
+        link: '/guide/',
+      },
+      {
+        text: '下载',
+        link: '/download/',
+      },
+    ],
+    logo: '/images/icon.png',
+    sidebar: [
+      // SidebarItem
+      {
+        text: '主界面',
+        link: '/guide/mainview/',
+      },
+      {
+        text: '中心线提取',
+        link: '/guide/centerline/',
+      },
+      {
+        text: '斑块分析',
+        link: '/guide/contour/',
+      },
+      {
+        text: '图像分割',
+        link: '/guide/seg/',
+      },
+      {
+        text: '下载',
+        link: '/download/',
+      },
+    ],
+  }),
 }
---
+++ docs/download/README.md
@@ -0,0 +1,9 @@
+# 下载
+
+下载MacOS 苹果芯片版本
+
+<a href='https://www.jianguoyun.com/p/DTFQQDQQ-MmzBhiijf4D'>MacOS</a>
+
+下载Windows版本
+
+<a href='https://www.jianguoyun.com/p/DcBxZ0IQ-MmzBhjhjf4D'>Windows 10</a>
