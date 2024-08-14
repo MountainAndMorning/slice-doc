99bd0739f51f0d17394bd704d6a63946125fa086
MountainAndMorning <2457065285@qq.com>
2024-08-14 21:46:31 +0800

    调整base

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/slice/",
+  base: "/slice-doc/",
   title: "TIMESlice",
   srcDir: 'docs',
   vite: {     // <=== insert this section
